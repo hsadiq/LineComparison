739eda63e65db1f1e28395a707d4c578acf6824c
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-13 02:02:45 +0530

    Added the code to LineComparison

---
+++ src/com/bl/LineCompare.java
@@ -0,0 +1,25 @@
+package com.bl;
+
+import java.util.Scanner;
+
+public class LineCompare {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Programme");
+
+        Scanner ab = new Scanner(System.in);
+
+        System.out.println("Enter X1 Value: ");
+        double x1 = ab.nextDouble();
+        System.out.println("Enter Y1 Value: ");
+        double y1 = ab.nextDouble();
+        System.out.println("Enter X2 Value: ");
+        double x2 = ab.nextDouble();
+        System.out.println("Enter Y2 Value: ");
+        double Y2 = ab.nextDouble();
+
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(Y2 - y1, 2));
+
+        System.out.println("The Length of Line Is: " + length);
+
+    }
+}
