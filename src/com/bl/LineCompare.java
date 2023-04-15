30c263787db73311e181314af1247baa58030f82
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-15 10:20:05 +0530

    Added the Code of Second Line

--- src/com/bl/LineCompare.java
+++ src/com/bl/LineCompare.java
@@ -17,9 +17,29 @@ public class LineCompare {
         System.out.print("Enter Y2 Value: ");
         double Y2 = ab.nextDouble();
 
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(Y2 - y1, 2));
+        /* Calculating the Line 1 Length UC-1 */
 
-        System.out.println("The Length of Line Is: " + length);
+        double length1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(Y2 - y1, 2));
+
+        System.out.println("The Length of Line 1 Is: " + length1);
+
+        System.out.print("Enter X1 Value: ");
+        double a1 = ab.nextDouble();
+        System.out.print("Enter Y1 Value: ");
+        double b1 = ab.nextDouble();
+        System.out.print("Enter X2 Value: ");
+        double a2 = ab.nextDouble();
+        System.out.print("Enter Y2 Value: ");
+        double b2 = ab.nextDouble();
+
+        /* Calculating the Line 2 Length UC-2 */
+
+        double length2 = Math.sqrt((Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)));
+
+        System.out.println("The Length of Line 2 is: " + length2);
+
+
+
 
     }
 }
