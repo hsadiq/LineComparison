245072716df44af10c3181be078a41be5b31fc5b
SADIQUE HUSSAIN <44770680+hsadiq@users.noreply.github.com>
2023-04-20 22:35:53 +0530

    Adding Refactor code of LineComparision

--- src/com/bl/LengthCompare.java
+++ src/com/bl/LengthCompare.java
@@ -1,4 +1,56 @@
 package com.bl;
 
+
+import java.util.Scanner;
+
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
 public class LengthCompare {
+    private Point startPoint;
+    private Point endPoint;
+
+    public LengthCompare(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    public double length() {
+        int xDiff = startPoint.getX() - endPoint.getX();
+        int yDiff = startPoint.getY() - endPoint.getY();
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the coordinates of the start point: ");
+        int startX = scanner.nextInt();
+        int startY = scanner.nextInt();
+
+        System.out.println("Enter the coordinates of the end point: ");
+        int endX = scanner.nextInt();
+        int endY = scanner.nextInt();
+
+        Point startPoint = new Point(startX, startY);
+        Point endPoint = new Point(endX, endY);
+
+        LengthCompare line = new LengthCompare(startPoint, endPoint);
+        System.out.println("The length of the line is " + line.length());
+    }
 }
